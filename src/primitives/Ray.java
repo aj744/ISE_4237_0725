b088df26dc737c7b166e4328009bd37b34268d20
aj744 <ariel.journ@gmail.com>
2025-03-24 18:42:10 +0200

    Added JavaDoc to primitives

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,11 +1,28 @@
 package primitives;
 
+/**
+ * Represents a ray in 3D space, defined by a starting point and a direction vector.
+ */
 public class Ray {
+    /**
+     * The direction vector of the ray. It is always normalized.
+     */
     private Vector vector;
+
+    /**
+     * The starting point of the ray.
+     */
     private Point point;
 
+    /**
+     * Constructs a Ray with a given direction vector and starting point.
+     * The direction vector is automatically normalized.
+     *
+     * @param vector The direction vector of the ray
+     * @param point The starting point of the ray
+     */
     public Ray(Vector vector, Point point) {
-        this.vector = vector.normalize();
+        this.vector = vector.normalize(); // Ensure the direction vector is normalized
         this.point = point;
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,35 +1,85 @@
 package primitives;
 
+/**
+ * Represents a 3D vector, which is a directed quantity in space.
+ * This class extends {@link Point} but ensures that a vector cannot be the zero vector.
+ */
 public class Vector extends Point {
 
-    private void checkIfZero(Double3 xyz){
-        if(xyz.equals(Double3.ZERO))
+    /**
+     * Checks if the given {@link Double3} value is zero and throws an exception if it is.
+     * @param xyz The coordinates to check
+     * @throws IllegalArgumentException if the vector is the zero vector
+     */
+    private void checkIfZero(Double3 xyz) {
+        if (xyz.equals(Double3.ZERO))
             throw new IllegalArgumentException("Vector can not be ZERO");
     }
+
+    /**
+     * Constructs a Vector with the given x, y, and z components.
+     * @param x X component
+     * @param y Y component
+     * @param z Z component
+     * @throws IllegalArgumentException if the vector is the zero vector
+     */
     public Vector(double x, double y, double z) {
         super(x, y, z);
         checkIfZero(new Double3(x, y, z));
-
     }
+
+    /**
+     * Constructs a Vector from a {@link Double3} object.
+     * @param xyz A {@link Double3} object representing the vector's components
+     * @throws IllegalArgumentException if the vector is the zero vector
+     */
     public Vector(Double3 xyz) {
         super(xyz);
         checkIfZero(xyz);
     }
+
+    /**
+     * Adds another vector to this vector and returns the resulting vector.
+     * @param vector The vector to add
+     * @return A new {@link Vector} representing the sum
+     * @throws IllegalArgumentException if the vectors cancel each other out
+     */
     public Vector add(Vector vector) {
-        if(!vector.equals(vector.scale(-1)))
+        if (!vector.equals(vector.scale(-1))) // Ensures vectors do not cancel each other out
             return new Vector(this.xyz.add(vector.xyz));
         else
             throw new IllegalArgumentException("Vectors can not be added");
     }
+
+    /**
+     * Scales this vector by a given scalar value.
+     * @param rhs The scalar value to multiply by
+     * @return A new {@link Vector} representing the scaled vector
+     * @throws IllegalArgumentException if the scalar is zero
+     */
     public Vector scale(double rhs) {
-        if(rhs != 0)
+        if (rhs != 0)
             return new Vector(this.xyz.scale(rhs));
         else
             throw new IllegalArgumentException("Scalar cant be ZERO");
     }
+
+    /**
+     * Computes the dot product of this vector with another vector.
+     * @param vector The other vector
+     * @return The dot product result as a double
+     */
     public double dotProduct(Vector vector) {
-        return this.xyz.product(vector.xyz).d1() + this.xyz.product(vector.xyz).d2() + this.xyz.product(vector.xyz).d3();
+        return this.xyz.product(vector.xyz).d1() +
+                this.xyz.product(vector.xyz).d2() +
+                this.xyz.product(vector.xyz).d3();
     }
+
+    /**
+     * Computes the cross product of this vector with another vector.
+     * @param vector The other vector
+     * @return A new {@link Vector} representing the cross product result
+     */
     public Vector crossProduct(Vector vector) {
         return new Vector(
                 this.xyz.d2() * vector.xyz.d3() - this.xyz.d3() * vector.xyz.d2(),
@@ -37,14 +87,28 @@ public class Vector extends Point {
                 this.xyz.d1() * vector.xyz.d2() - this.xyz.d2() * vector.xyz.d1()
         );
     }
+
+    /**
+     * Computes the squared length (magnitude) of the vector.
+     * @return The squared length of the vector
+     */
     public double lengthSquared() {
         return this.dotProduct(this);
     }
+
+    /**
+     * Computes the length (magnitude) of the vector.
+     * @return The length of the vector
+     */
     public double length() {
         return Math.sqrt(this.lengthSquared());
     }
-    public Vector normalize() {
-        return this.scale(1/this.length());
-    }
 
+    /**
+     * Normalizes this vector to have a length of 1.
+     * @return A new {@link Vector} representing the normalized vector
+     */
+    public Vector normalize() {
+        return this.scale(1 / this.length());
+    }
 }
