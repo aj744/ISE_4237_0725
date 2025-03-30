dfeb8407b9d1729f36dcf4fd25c8a5f753231946
aj744 <ariel.journ@gmail.com>
2025-03-30 18:35:56 +0300

    Added comments to  Sphere.java

--- src/unittests/geometries/SphereTests.java
+++ src/unittests/geometries/SphereTests.java
@@ -7,6 +7,8 @@ import primitives.Point;
 import primitives.Vector;
 
 public class SphereTests {
+    // ============ Equivalence Partitions Tests ==============
+    // TC01: get a normal for some point (All points are the same)
     @Test
     public void TestGetNormal() {
         Sphere sphere = new Sphere(1, new Point(0, 0, 0));
