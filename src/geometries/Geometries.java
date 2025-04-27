5c32ae478522a98dabbf6282f6f6c3296f44312a
aj744 <ariel.journ@gmail.com>
2025-04-27 22:37:58 +0300

    added Geometries class

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,35 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Geometries implements Intersectable {
+    private final List<Intersectable> geometries = new LinkedList<>();
+
+    public Geometries(Intersectable... geometries) {
+        add(geometries);
+    }
+
+    public void add(Intersectable... geometries) {
+        this.geometries.addAll(Arrays.asList(geometries));
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        List<Point> intersections = null;
+        for (Intersectable geometry : this.geometries) {
+            List<Point> geometryIntersections = geometry.findIntersections(ray);
+            if (geometryIntersections != null) {
+                if (intersections == null) {
+                    intersections = new LinkedList<>();
+                }
+                intersections.addAll(geometryIntersections);
+            }
+        }
+        return intersections;
+    }
+}
