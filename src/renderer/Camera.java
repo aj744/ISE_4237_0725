86a4d641df36b593181c48314a4f2da17da982d2
YAIR1-1 <yairrot2007@gmail.com>
2025-05-03 22:39:13 +0300

    Moved unittests outside of src

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -7,6 +7,8 @@ import primitives.Util;
 
 import java.util.MissingResourceException;
 
+import static primitives.Util.alignZero;
+
 public class Camera implements Cloneable{
     public static class Builder {
         final Camera camera = new Camera();
@@ -17,19 +19,20 @@ public class Camera implements Cloneable{
         }
 
         public Builder setDirection(Vector up, Vector to){
-            if (up.dotProduct(to) != 0) throw new IllegalArgumentException("Vectors up and to are not orthogonal");
-
+            if(checkOrthogonal(up,to)){
+                throw new IllegalArgumentException("Up and To must be orthogonal");
+            }
             camera.up = up.normalize();
             camera.to = to.normalize();
             camera.right = camera.up.crossProduct(camera.to).normalize();
-
             return this;
         }
 
         public Builder setDirection(Point target, Vector up){
             Vector to = target.subtract(camera.location);
-            if (up.dotProduct(to) != 0) throw new IllegalArgumentException("Vectors up and to are not orthogonal");
-
+            if (checkOrthogonal(up,to)){
+                throw new IllegalArgumentException("Up and To must be orthogonal");
+            }
             camera.to = to.normalize();
             camera.right = to.crossProduct(up).normalize();
 
@@ -45,16 +48,18 @@ public class Camera implements Cloneable{
         }
 
         public Builder setVpSize(double height, double width) {
-            if (height  <= 0) throw new IllegalArgumentException("height must be grater than zero");
-            if (width <= 0) throw new IllegalArgumentException("width must be grater than zero");
+            if (checkGreaterThanZero(height))
+                throw new IllegalArgumentException("height must be grater than zero");
+            if (checkGreaterThanZero(width))
+                throw new IllegalArgumentException("width must be grater than zero");
             camera.height = height;
             camera.width = width;
-
             return this;
         }
 
         public Builder setVpDistance(double distance) {
-            if (distance <= 0) throw new IllegalArgumentException("distance must be grater than zero");
+            if (checkGreaterThanZero(distance))
+                throw new IllegalArgumentException("distance must be grater than zero");
             camera.distance = distance;
 
             return this;
@@ -65,11 +70,20 @@ public class Camera implements Cloneable{
         }
 
         public Camera build() {
-            if (camera.location.equals(Point.ZERO)) throw new MissingResourceException("the camera location is not set");
-
+            if (checkGreaterThanZero(camera.distance))
+                throw new IllegalArgumentException("distance must be grater than zero");
+            if (checkGreaterThanZero(camera.height))
+                throw new IllegalArgumentException("height must be grater than zero");
+            if (checkGreaterThanZero(camera.width))
+                throw new IllegalArgumentException("width must be grater than zero");
+            if(checkOrthogonal(camera.up,camera.to))
+                throw new IllegalArgumentException("Vectors up and to are not orthogonal");
 
             return (Camera) this.camera.clone();
         }
+
+        public boolean checkOrthogonal(Vector a, Vector b) {return a.dotProduct(b) != 0 ;}
+        public boolean checkGreaterThanZero(double a) {return alignZero(a) <= 0;}
     }
 
     private Point location;
