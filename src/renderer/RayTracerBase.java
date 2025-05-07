1d6f79bf996189741955008667b2a1877cd2516d
YAIR1-1 <yairrot2007@gmail.com>
2025-05-07 18:18:39 +0300

    finish class renderer

---
+++ src/renderer/RayTracerBase.java
@@ -0,0 +1,17 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+public abstract class RayTracerBase {
+    protected final Scene scene;
+
+    protected RayTracerBase(Scene scene) {
+        this.scene = scene;
+    }
+
+    public abstract Color traceRay(Ray ray){
+        return ray.getIntensity();
+    }
+}
