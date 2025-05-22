f33dc29338597021221db68ea48a60bd10a76bfd
aj744 <ariel.journ@gmail.com>
2025-05-22 17:13:59 +0300

    added changes to simpleRayTracer

--- src/renderer/SimpleRayTracer.java
+++ src/renderer/SimpleRayTracer.java
@@ -3,6 +3,7 @@ package renderer;
 import primitives.Color;
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 import scene.Scene;
 
 import java.util.List;
@@ -46,5 +47,9 @@ public class SimpleRayTracer extends RayTracerBase {
      */
     private Color calcColor(Point point) {
         return scene.ambientLight.getIntensity();
+    }
+
+    public boolean preprocessIntersection(Intersection intersection, Vector ray) {
+
     }
 }
